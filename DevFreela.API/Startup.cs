99cd90c237d2d42269b19d3c9050edde57e788de
paulostevo <41132104+paulostevo@users.noreply.github.com>
2022-05-20 13:03:31 -0300

    Creating validation with FluentValidation

--- DevFreela.API/Startup.cs
+++ DevFreela.API/Startup.cs
@@ -1,7 +1,9 @@
 using DevFreela.Application.Commands.CreateProject;
+using DevFreela.Application.Validators;
 using DevFreela.Core.Repositories;
 using DevFreela.Infrasctructure.Persistence;
 using DevFreela.Infrastructure.Persistence.Repositories;
+using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -38,7 +40,8 @@ namespace DevFreela.API
 
             services.AddMediatR(typeof(CreateProjectCommand));
 
-            services.AddControllers();
+            services.AddControllers()
+                    .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CreateUserCommandValidator>());
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DevFreela.API", Version = "v1" });
---
+++ DevFreela.Application/Validators/CreateUserCommandValidator.cs
@@ -0,0 +1,37 @@
+﻿using DevFreela.Application.Commands.CreateUser;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Validators
+{
+    public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserCommandValidator()
+        {
+            RuleFor(p => p.Email)
+                .EmailAddress()
+                .WithMessage("Email não válido");
+
+            RuleFor(p => p.Password)
+                .Must(ValidPassword)
+                .WithMessage("Senha deve conter no mínimo 8 caracteres, um número, uma maiúscula ,uma minúscula e um caractere especial");
+
+            RuleFor(p => p.FullName)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Nome é obrigatório");
+        }
+
+        public bool ValidPassword(string password)
+        {
+            var regex = new Regex(@"^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$");
+
+            return regex.IsMatch(password);
+        }
+    }
+}
